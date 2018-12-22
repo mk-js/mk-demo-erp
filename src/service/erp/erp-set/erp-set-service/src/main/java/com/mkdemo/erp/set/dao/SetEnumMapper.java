ed89d24ca5142656bfa72f8ec609d50357848890
lsg <lsg@rrtimes.com>
2018-12-22 12:33:15 +0800

    init

---
+++ src/service/erp/erp-set/erp-set-service/src/main/java/com/mkdemo/erp/set/dao/SetEnumMapper.java
@@ -0,0 +1,20 @@
+package com.mkdemo.erp.set.dao;
+
+import com.mk.eap.entity.dao.EntityMapper;
+import com.mkdemo.erp.set.vo.SetEnumVo;
+
+/**
+ * @Title:       SysSetEnumMapper.java
+ * @Package:     com.mkdemo.erp.set.dao
+ * @Description: 类文件概述
+ * 
+ * <p>
+ * 	类文件详细描述
+ * </p> 
+ * 
+ * @author lsg
+ * 
+ */
+public interface SetEnumMapper  extends EntityMapper<SetEnumVo>{
+
+}
---
+++ src/service/erp/erp-sys/erp-sys-service/src/main/java/com/mkdemo/erp/sys/dao/SysTaskMapper.java
@@ -0,0 +1,20 @@
+package com.mkdemo.erp.sys.dao;
+
+import com.mk.eap.entity.dao.EntityMapper;
+import com.mkdemo.erp.sys.vo.SysTaskVo;
+
+/**
+ * @Title:       SysSysTaskMapper.java
+ * @Package:     com.mkdemo.erp.sys.dao
+ * @Description: 类文件概述
+ * 
+ * <p>
+ * 	类文件详细描述
+ * </p> 
+ * 
+ * @author lsg
+ * 
+ */
+public interface SysTaskMapper  extends EntityMapper<SysTaskVo>{
+
+}
